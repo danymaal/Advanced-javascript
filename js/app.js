2b1b7aa33b3412712b388de86cc9421d4484ed63
Dany <deeelorxxd@gmail.com>
2022-02-18 19:32:19 +0200

    async await: done

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ delay(2000).then(() => {
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 function fetchTODOs() {
-    console.log('Fetch todo has been started');
+  console.log('Fetch todo has been started');
   return delay(2000)
     .then(() => {
       return fetch(url);
@@ -32,11 +32,25 @@ fetchTODOs()
     console.error(e);
   });
 
-
-
-  async function fetchAsyncTodos () {
-    await delay(2000)
+async function fetchAsyncTodos() {
+  console.log('Fetch todo has been started');
+  try {
+    await delay(2000);
+    const response = await fetch(url);
+    // Тот результат, который попадает в метод then, мы можем просто получить переменную
+    const data = await response.json();
+    console.log('Data', data);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log('Finally');
   }
+}
 
-   // Те функции, которые внутри принимают await, должны быть асинхронными
-//    Мы знаем, что наша функция delay возвращает промис, но учитывая, что наша функция сейчас асинхронная, вместо того чтобы обрабатывать delay с помощью then, мы можем использовать await
+fetchAsyncTodos();
+
+// Те функции, которые внутри принимают await, должны быть асинхронными
+// Мы знаем, что наша функция delay возвращает промис, но учитывая, что наша функция сейчас асинхронная, вместо того чтобы обрабатывать delay с помощью then, мы можем использовать await
+// Следущей строчкой мы можем выполнять какую то логику, важный момент заключается в том, что мы не перейдем к этой строчке пока данный промис не зарезолвится (выполнится)
+
+// async await это так называемый синтаксический сахар, который позволяет нам более удобно программировать какие то асинхронные вещи
